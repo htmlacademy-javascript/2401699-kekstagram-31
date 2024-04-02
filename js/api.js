9a31d8995d1c5c3f26f0a413bdde6450f2967896
Лиана <verbovskayaliana@gmail.com>
2024-04-02 13:33:29 +0300

    вносит правки

--- js/api.js
+++ js/api.js
@@ -1,35 +1,35 @@
-const BASE_URL = 'https://31.javascript.htmlacademy.pro/kekstagram';
+// const BASE_URL = 'https://31.javascript.htmlacademy.pro/kekstagram';
 
-const Route = {
-  GET_DATA: '/data',
-  SEND_DATA: '/',
-};
+// const Route = {
+//   GET_DATA: '/data',
+//   SEND_DATA: '/',
+// };
 
-const Method = {
-  GET: 'GET',
-  POST: 'POST',
-};
+// const Method = {
+//   GET: 'GET',
+//   POST: 'POST',
+// };
 
-const ErrorText = {
-  GET_DATA: 'Не удалось загрузить данные. Попробуйте обновить страницу',
-  SEND_DATA: 'Не удалось отправить форму. Попробуйте ещё раз',
-};
+// const ErrorText = {
+//   GET_DATA: 'Не удалось загрузить данные. Попробуйте обновить страницу',
+//   SEND_DATA: 'Не удалось отправить форму. Попробуйте ещё раз',
+// };
 
-const load = (route, errorText, method = Method.GET, body = null) =>
-  //м который отпр на сервер({}конкатеаця происходит)
-  fetch(`${BASE_URL}${route}`, {method, body})
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error();
-      }
-      return response.json();
-    })
-    .catch(() => {
-      throw new Error(errorText);
-    });
+// const load = (route, errorText, method = Method.GET, body = null) =>
+//   //м который отпр на сервер({}конкатеаця происходит)
+//   fetch(`${BASE_URL}${route}`, {method, body})
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error();
+//       }
+//       return response.json();
+//     })
+//     .catch(() => {
+//       throw new Error(errorText);
+//     });
 
-const getData = () => load(Route.SEND_DATA, ErrorText.GET_DATA);
+// const getData = () => load(Route.SEND_DATA, ErrorText.GET_DATA);
 
-const sendData = (body) => load(Route.SEND_DATA, ErrorText.SEND_DATA, Method.POST, body);
+// const sendData = (body) => load(Route.SEND_DATA, ErrorText.SEND_DATA, Method.POST, body);
 
-export { getData, sendData };
+// export { getData, sendData };
