869fd19c9266533b80bbed8a12032ebfb089013b
Лиана <verbovskayaliana@gmail.com>
2024-04-02 12:01:27 +0300

    сохраняем изменения чтобы вернуться на другую ветку и сделать синхронизацию

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@ import './render-comments.js';
 import { openPicture } from './render-photo.js';
 import { initUploadModal } from './form.js';
 import './valid-hashtag.js';
+import { similarPhoto } from './thumbnails.js';
+import { getData } from './api.js';
+
+const dataError = document.querySelector('#data-error').content.querySelector('.data-error');
+
+getData()
+  .then((photos) => {
+    similarPhoto(photos);
+  }).catch(() => {
+    document.body.append(dataError);
+    setTimeout(() => document.body.removeChild(dataError), 5000);
+  });
 
 openPicture();
 initUploadModal();
